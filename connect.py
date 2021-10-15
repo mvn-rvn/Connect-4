21e66fdef014ace2a391a8a463bf1a614bd338ee
Dewahyu Nathan <F5.SH@pcsb.org>
2021-10-15 13:51:04 -0400

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,9 +1,17 @@
-#initialize the 2d array
+# initialize the 2d array
 grid = []
 while len(grid) != 6:
-    grid.append(["#", "#", "#", "#", "#", "#", "#"])
+    grid.append(["-", "-", "-", "-", "-", "-", "-"])
 
-for row in grid:
-    for elem in row:
-        print(elem, end="")
-    print(" ")
+# initialize turn order
+turn = "X"
+
+# REMINDER: grid[y][x]. y coordinate goes from top to bottom
+def printgrid():
+    print("\n1 2 3 4 5 6 7")
+    for row in grid:
+        for elem in row:
+            print(elem + " ", end="")
+        print(" ")
+    print(turn + "'s turn")
+
