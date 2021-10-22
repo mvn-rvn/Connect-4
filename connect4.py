031ddd8529905aece496f5d2150cb80b6f84e4b9
mvn-rvn <67172473+mvn-rvn@users.noreply.github.com>
2021-10-22 00:21:32 +0000

    Changed blue pieces to yellow

--- connect4.py
+++ connect4.py
@@ -98,7 +98,7 @@ async def gameloop():
             break
         #advance turn order
         if turn == "\N{LARGE RED CIRCLE}":
-            turn = "\N{LARGE BLUE CIRCLE}"
+            turn = "\N{LARGE YELLOW CIRCLE}"
         else:
             turn = "\N{LARGE RED CIRCLE}"
         data = None
